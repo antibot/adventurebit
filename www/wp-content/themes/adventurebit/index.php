49280a0eefa433fc294af2c8a775e6ad8d52920d
Dmitry Selichov <respect_man@mail.ru>
2011-12-09 14:32:41 +0400

    -

---
+++ www/wp-content/themes/adventurebit/index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+		<div id="container">
+		  <?php
+        get_template_part( 'loop', 'index' );
+      ?>
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
