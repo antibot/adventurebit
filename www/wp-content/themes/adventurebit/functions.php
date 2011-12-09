6585cd3770242396675365effc78c51909ca4d40
Dmitry Selichov <respect_man@mail.ru>
2011-12-09 12:19:55 +0400

    -

---
+++ www/wp-content/themes/adventurebit/functions.php
@@ -0,0 +1,14 @@
+<?php
+
+/* menu
+----------------------------------------------------------------------------- */
+
+if(function_exists('add_theme_support')) {
+  add_theme_support('menus');
+}
+
+register_nav_menu('main', 'Главное меню');
+
+
+
+?>
---
+++ www/wp-content/themes/adventurebit/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+		<div id="container">
+		  123
+		</div><!-- #container -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
