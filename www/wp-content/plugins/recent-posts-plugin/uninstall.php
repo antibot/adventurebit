c36dc8c3aee98b3af43e4019bfc678a224ce6815
Dmitry Selichov <respect_man@mail.ru>
2011-12-12 18:13:11 +0400

    -

--- www/wp-content/plugins/recent-posts-plugin/uninstall.php
+++
@@ -1,7 +0,0 @@
-<?php
-	if (defined('ABSPATH') && defined('WP_UNINSTALL_PLUGIN') 
-	&& strtolower(WP_UNINSTALL_PLUGIN) === 'recent-posts-plugin/recent-posts.php') {
-		delete_option('recent-posts');
-		delete_option('widget_rrm_recent_posts');
-	}
-?>
