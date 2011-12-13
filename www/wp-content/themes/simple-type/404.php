134a4ac34a6d108b8844242f11e3c8bdcc473443
Dmitry Selichov <respect_man@mail.ru>
2011-12-13 17:03:46 +0400

    -

--- www/wp-content/themes/simple-type/404.php
+++ www/wp-content/themes/simple-type/404.php
@@ -1,29 +1,20 @@
 <?php get_header(); ?>
 
-
-
 <div id="container">
 
-
-
-<div class="post">
-
-
-
-<h2>Oops&#133; 404&#133; </h2>
-
-
-
-<p>It looks like you tried to access a page that isn't here anymore. It's either you that did something wrong, or it's us. Either way, let's not waste any more time on this page since there's nothing more to see here.</p>
-
-
+  <div class="post">
+  <h2>Oops&#133; 404&#133; </h2>
+    <p>
+      It looks like you tried to access a page that isn't here anymore. 
+      It's either you that did something wrong, or it's us. 
+      Either way, let's not waste any more time on this page since there's nothing more to see here.
+    </p>
+  </div>
 
 </div>
 
-		</div>
-
 <?php get_sidebar(); ?>
 
-		</div>
+</div>
 
 <?php get_footer() ?>
